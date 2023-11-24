153a04df9ea3a8f752e816cc03cbbf9a546507ba
SANJAY-KUMAR-97 <147901074+SANJAY-KUMAR-97@users.noreply.github.com>
2023-11-24 23:30:20 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,7 +4,7 @@ public class Main {
    public static void main(String args[]) {
         RWOnly name = new RWOnly();
 
-        name.setName("Nikith Kumar N");
+        name.setName("SANJAY KUMAR");
 
         System.out.println(name.getName());
     }
